4c5bc3cd33ce1dbbde830861a5df513b16b977cf
anton95i <anton.steiner@hotmail.com>
2021-06-30 19:57:46 +0200

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Starting REST Service...");
+        ResourceConfig rc = new ResourceConfig().packages("org.example");
+        HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), rc);
+        System.out.println("REST Service successfully started!");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.stop(0);
+        System.out.println("Service stopped!");
+    }
+
+
+}
